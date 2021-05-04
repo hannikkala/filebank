99d7a36069f9247f3aceebefe7237cb45cf1df5f
Tommi Hännikkälä <tommi.hannikkala@wunderdog.fi>
2021-05-04 12:11:25 +0300

    Removed prettier/@typescript-eslint from eslintrc

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -9,8 +9,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint"
+    "plugin:@typescript-eslint/recommended"
   ],
   rules: {
     "no-case-declarations": "off",
